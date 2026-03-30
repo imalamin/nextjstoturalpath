b22e3892151d8312aaf69c779da0e6847d0ab392
mdamin.it <alamin.it@pubalibankbd.com>
2026-03-30 17:50:53 +0600

    DynamicRouting

---
+++ app/dashboard/users/[id]/page.tsx
@@ -0,0 +1,13 @@
+
+const UserDetails = async(   {params}:{params: Promise<{ id: string }>}) => {
+const {id} = await params;
+
+  return (
+    <div>
+      <h1>User Details for User {id}</h1>
+
+    </div>
+  )
+}
+
+export default UserDetails
